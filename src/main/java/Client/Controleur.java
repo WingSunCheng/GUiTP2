49c59990ef29ec7aa0e0028c8f15fe42e91aca14
WingSunCheng <wingsuncheng09@gmail.com>
2023-04-13 16:08:35 -0400

    Controleur class

--- src/main/java/Client/Controleur.java
+++ src/main/java/Client/Controleur.java
@@ -1,4 +1,27 @@
 package Client;
 
+import server.models.Course;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Controleur {
+	private Modele modele;
+	ArrayList<Course> courses = new ArrayList<>();
+
+	public ArrayList<Course> getCourses() {
+		return courses;
+	}
+
+	public Controleur(Modele modele) {
+		this.modele = modele;
+	}
+	public void charger(String session) throws IOException {
+		this.modele.getCourses(session);
+		this.courses = this.modele.getCourse();
+	}
+
+	public void inscripting(String prenom, String nom, String email, String matricule,Course chosenClass){
+		this.modele.getInscription(prenom,nom,email,matricule,chosenClass);
+	}
 }
