e08457abcc9e3140d5fac862faa959a957e86a59
estelleZong <estellezong9@gmail.com>
2023-04-15 13:41:54 -0400

    javadoc Vue and Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 import Client.Vue;
 
-
-
-
 public class Main {
 	/**
 	 * Cette méthode appelle simplement la classe qui étend l'application pour pouvoir créer un jar fonctionnel
@@ -10,7 +7,8 @@ public class Main {
 	 *
 	 * @param args argument de ligne de commande mais est ignoré,uniquement utilisé
 	 *                pour pouvoir appeler la méthode main depuis Vue, pour lancer l'application
-	 */public static void main(String[] args) {
+	 */
+	public static void main(String[] args) {
 		Vue.main(args);
 	}
 }
